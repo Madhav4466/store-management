20f80d0d45d2b4d9cc9b34d922b7c4b51d5608ad
Madhav Saraf <madhavsaraf183@gmail.com>
2025-01-10 16:22:14 +0530

    Create app by adding necessary dependencies

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import './App.css';
+import LoginNew from './pages/account/login-new';
+import Dashboard from './pages/dashboard/dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NewUser from './pages/account/new-user';
+import ForgotPassword from './pages/account/forgot';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
+    },
+    {
+      path: 'account',
+      children: [
+        {
+          path: 'login',
+          element: <LoginNew />
+        },
+        {
+          path: 'create',
+          element: <NewUser />
+        },
+        {
+          path: 'forgot',
+          element: <ForgotPassword />
+        }
+      ]
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <div className="row vh-100">
+          <RouterProvider router={router}></RouterProvider>
+          <Outlet/>
+        </div>
+      </div>
+    </div>
+  );
+}
+//https://dribbble.com/tags/health-care-dashboard
+//https://www.youtube.com/watch?v=Zujd8BSCJ2U
+export default App;
